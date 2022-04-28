72fbe0bffceae5234a620165bc0b1fc5f441b7d6
Luis Chilo <gfxargentina@gmail.com>
2022-04-28 14:36:30 -0300

    frontend backend basic setup

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+
+const app = express();
+
+//para limitar el tamaño del envio de imagenes
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+dotenv.config();
+const CONNECTION_URL = process.env.DB;
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Servidor corriendo en el puerto: ${PORT}`)
+    )
+  )
+  .catch((error) => console.log(error.message));
