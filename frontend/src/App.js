45a1c5fd454a0af0e01caa0365492b4a694569cc
Luis Chilo <gfxargentina@gmail.com>
2022-04-28 21:28:01 -0300

    add tailwindcss daisy ui kit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,25 +4,23 @@ import memories from "./images/memories.png";
 
 function App() {
   return (
-    <div>
-      <div>
-        <div>Memories</div>
-        <img className="" src={memories} alt="memories" height="60" />
-      </div>
-      <div in>
-        <divainer>
-          <div>
-            <div>
-              <Posts />
-            </div>
-
-            <div>
-              <Form />
-            </div>
+    <>
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <figure>
+          <img
+            src="https://api.lorem.space/image/shoes?w=400&h=225"
+            alt="Shoes"
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">Shoes!</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Buy Now</button>
           </div>
-        </divainer>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
