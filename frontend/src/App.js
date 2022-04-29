67a619fb207507e5f16da984c80cb5576b3c790a
Luis Chilo <gfxargentina@gmail.com>
2022-04-28 21:00:27 -0300

    delete all mui styles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,27 @@
-import "./App.css";
+import Form from "./components/Form/Form";
+import Posts from "./components/Posts/Posts";
+import memories from "./images/memories.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>memories</h1>
-      </header>
+    <div>
+      <div>
+        <div>Memories</div>
+        <img className="" src={memories} alt="memories" height="60" />
+      </div>
+      <div in>
+        <divainer>
+          <div>
+            <div>
+              <Posts />
+            </div>
+
+            <div>
+              <Form />
+            </div>
+          </div>
+        </divainer>
+      </div>
     </div>
   );
 }
