362a407fdfa6538765528fb0113cf1d7522382cd
Luis Chilo <gfxargentina@gmail.com>
2022-06-01 18:48:53 -0300

    change token expire duration

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -23,7 +23,7 @@ export const signin = async (req, res) => {
     const token = jwt.sign(
       { email: existingUser.email, id: existingUser._id },
       process.env.SECRET,
-      { expiresIn: "2h" }
+      { expiresIn: "1h" }
     );
 
     res.status(200).json({ result: existingUser, token });
