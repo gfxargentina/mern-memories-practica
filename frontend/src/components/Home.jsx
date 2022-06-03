ede097f5b0899dfbdaf06853ed3b6bb98559b148
Luis Chilo <gfxargentina@gmail.com>
2022-06-03 11:28:45 -0300

    add search basic structure

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -7,13 +7,48 @@ import { getPosts } from "../actions/posts";
 import Form from "./Form/Form";
 import Posts from "./Posts/Posts";
 
+import { useNavigate, useLocation } from "react-router-dom";
+
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
+}
+
 const Home = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const query = useQuery();
+
+  const page = query.get("page") || 1;
+  const searchQuery = query.get("searchQuery");
+  const [search, setSearch] = useState("");
+  const [tags, setTags] = useState([]);
+
   const [currentId, setCurrentId] = useState(null);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(getPosts());
   }, [currentId, dispatch]);
+
+  const searchPost = () => {
+    if (search.trim()) {
+      //disptach logic for fetch search post
+    }
+  };
+
+  const handleKeyPress = (e) => {
+    //si aprieta la tecla enter realiza la busqueda
+    if (e.keyCode === 13) {
+      //search post
+      searchPost();
+    } else {
+      navigate("/");
+    }
+  };
+
+  //cuando se tiene un array de state, primero se usa el spread operator para añadir los tags previos
+  //y despues se agrega el tag nuevo al array
+  const handleAddTag = (tag) => setTags([...tags, tag]);
 
   return (
     <div className="flex p-10 justify-center">
@@ -23,6 +58,26 @@ const Home = () => {
         </div>
 
         <div className="grid grid-cols-1">
+          <input
+            name="search"
+            type="text"
+            onKeyPress={handleKeyPress}
+            placeholder="Buscar"
+            class="input w-full max-w-xs"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+
+          <input
+            type="text"
+            placeholder="Type here"
+            onChange={(e) => handleAddTag(e.target.value)}
+            class="input input-bordered w-full max-w-xs"
+          />
+          <button onClick={searchPost} class="btn btn-outline btn-primary">
+            Buscar
+          </button>
+
           <Form currentId={currentId} setCurrentId={setCurrentId} />
 
           <div className="mt-5">
